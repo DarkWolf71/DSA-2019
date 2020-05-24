db7a02db43501daaddd3b3878d1c4cf30bf7062b
DarkWolf71 <65805212+DarkWolf71@users.noreply.github.com>
2020-05-24 16:02:21 +0530

    Working

---
+++ 1.2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <cfloat>
+#include <iomanip>
+using namespace std;
+
+int main(){
+	int number;
+	cin>>number;
+	int n;
+	n=number;
+	int *a=new int[number];
+	int b=n*n-2*(n-1);
+	for(int i=0; i<n;i++){
+		a[i]=b--;
+	}
+	
+	
+	
+	
+	
+
+	for(int j=0; j<(number/2+1); j++){
+		for (int k=0; k<j; k++){
+			cout<<++a[k]<<"     ";
+		}
+		int b=n*n-2*(n-1);
+		for (int l=j; l<(number-j); l++){
+			a[l]=b--;
+			cout<<a[l]<<"     ";
+		}
+		for (int m=number-j; m<number; m++){
+			cout<<--a[m]<<"     ";
+		}
+		n-=2;
+		cout<<'\n'<<'\n';
+	}
+
+
+	for (int i=1;i<=number-(number/2)-1;i++){
+		int j=0;
+		for (; j<(number/2)  -i;j++){
+			cout<<++a[j]<<"     ";
+		}
+		int b=a[j];
+		for(;j< number/2 +1 +i; j++){
+			cout<<++b<<"     ";
+		}
+		for(;j<number; j++){
+			cout<<--a[j]<<"      ";
+		}
+		cout<<'\n'<<'\n';
+	}
+	
+	
+	return 0;
+}
